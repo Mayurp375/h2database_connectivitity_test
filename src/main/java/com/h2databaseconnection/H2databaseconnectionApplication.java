b43fbe0e8bb4e68e38336071622e1224fd3d8967
unknown <mayurpolojwar1234@gmail.com>
2023-06-12 13:28:15 +0530

    this is h2 data base connection test file

---
+++ src/main/java/com/h2databaseconnection/H2databaseconnectionApplication.java
@@ -0,0 +1,14 @@
+package com.h2databaseconnection;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2databaseconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2databaseconnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/h2databaseconnection/H2databaseconnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h2databaseconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2databaseconnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
